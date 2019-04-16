09b17ba9e3c88ad890831d72c55da4e0a11280a3
Elliot Moore <elliotmoorecornwall@outlook.com>
2019-04-16 14:00:58 +0100

    Update Settings.py
    Converted to class

--- Settings.py
+++ Settings.py
@@ -1,3 +1,9 @@
-ApiUrl = "https://api.tfl.gov.uk/"
-appid = "app_id=d83cbf0b"
-appkey = "app_key=486727de8027a1be9a212c5d5c2ae8df"
+import mysql.connector as mariadb
+
+
+class Settings:
+    ApiUrl = "https://api.tfl.gov.uk/"
+    appid = "app_id=d83cbf0b"
+    appkey = "app_key=486727de8027a1be9a212c5d5c2ae8df"
+    mariadb_connection = mariadb.connect(user='EMoore', password='Pa$$w0rd', database='FlaskAPIRequests')
+    cursor = mariadb_connection.cursor()
